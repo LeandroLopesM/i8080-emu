ce37dfdcea5f89badc48bb3f452efb4e3af0f14c
LeandroLopesM <leandrolopes.tinfem2023@gmail.com>
2026-04-11 00:38:40 -0300

    debug formatting

--- cpu/debug/debug.c
+++ cpu/debug/debug.c
@@ -4,12 +4,12 @@
 
 void dump_registers(cpu* c)
 {
-    printf("Core dump:\n");
-    printf("SP: %-4X, PC: %-4X\n", c->rgf.SP, c->rgf.PC);
-    printf("%d%d%d%d%d%d\n", c->rgf.S, c->rgf.Z, c->rgf.I, c->rgf.P, c->rgf.CY, c->rgf.AC);
-    printf("SZIPCYA\n");
-    printf("* General registers:\n");
-    printf("| A: %2X PSW: %-4X:\n", c->rgf.A, c->rgf.PSW);
+    printf("* Core dump:\n");
+    printf("| SP: %-4X PC: %-4X\n", c->rgf.SP, c->rgf.PC);
+    printf("| %d%d%d%d%d%d\n", c->rgf.S, c->rgf.Z, c->rgf.I, c->rgf.P, c->rgf.CY, c->rgf.AC);
+    printf("| SZIPCYA\n");
+    printf("+ General registers:\n");
+    printf("| PSW: %-4X A: %2X:\n", c->rgf.A, c->rgf.PSW);
     printf("| D: %2X E: %2X => DE: %-4X\n", c->rgf.D, c->rgf.E, c->rgf.DE);
     printf("| B: %2X C: %2X => BC: %-4X\n", c->rgf.B, c->rgf.C, c->rgf.BC);
     printf("| H: %2X L: %2X => HL: %-4X\n", c->rgf.H, c->rgf.L, c->rgf.HL);
